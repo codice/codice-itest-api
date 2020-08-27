fa730f08f5f96b2aa32498c75da7e2f73edb9dbd
Don Cruver <donald.cruver@connexta.com>
2020-08-27 09:36:00 -0700

    updated licensing

--- src/main/java/org/codice/itest/api/IntegrationTest.java
+++ src/main/java/org/codice/itest/api/IntegrationTest.java
@@ -1,14 +1,17 @@
 /**
- * Copyright 2020 Connexta, LLC
- *
- * Unlimited Government Rights (FAR Subpart 27.4) Government right to use, disclose, reproduce,
- * prepare derivative works, distribute copies to the public, and perform and display publicly, in
- * any manner and for any purpose, and to have or permit others to do so.
- **/
-package org.codice.integrationtest.api;
+ * Copyright (c) Codice Foundation
+
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * http://www.gnu.org/licenses/lgpl.html.
+ */
+package org.codice.itest.api;
 
 /**
- * integrationtest-framework will execute any instances of this interface in the application
+ * codice-itest will execute any instances of this interface in the application
  * context. Methods are called in the following order: setup(), test(), cleanup().
  */
 public interface IntegrationTest {
--- src/main/java/org/codice/itest/api/TestResultFactory.java
+++ src/main/java/org/codice/itest/api/TestResultFactory.java
@@ -1,16 +1,19 @@
 /**
- * Copyright 2020 Connexta, LLC
- *
- * Unlimited Government Rights (FAR Subpart 27.4) Government right to use, disclose, reproduce,
- * prepare derivative works, distribute copies to the public, and perform and display publicly, in
- * any manner and for any purpose, and to have or permit others to do so.
- **/
-package org.codice.integrationtest.api;
+ * Copyright (c) Codice Foundation
+
+ * This is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or any later version.
+
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public License is distributed along with this program and can be found at
+ * http://www.gnu.org/licenses/lgpl.html.
+ */
+package org.codice.itest.api;
 
 import java.time.Instant;
 
 /**
- * A factory interface for creating TestResult objects. integrationtest-framework provides a default implementation
+ * A factory interface for creating TestResult objects. codice-itest provides a default implementation
  * of this interface but it's included here so that its behavior can be customized.
  */
 public interface TestResultFactory {
